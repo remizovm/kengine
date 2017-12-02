6b38727fca1948efb3625aca74564141f9f19eb7
Mikhail Remizov <00keng@gmail.com>
2017-12-03 00:30:18 +0300

    - Add the main procedure - Add some comments - Replace the lea+push calls with the addr macro

--- src/kengine.asm
+++ src/kengine.asm
@@ -1,3 +1,5 @@
+; FILENAME: kengine.asm
+
 include 'win32axp.inc'
 
 include 'macros.inc'
@@ -5,25 +7,56 @@ include 'const.inc'
 include 'structs.inc'
 
 .code
+; main entry point
 start:
+        stdcall main
+        invoke  ExitProcess,eax
+
+; creates the main window and starts the main window loop
+proc main
+; in:  nothing
+; out: last window message status
+locals
+        ; window class structure
+        wc      WNDCLASS
+        ; window message
+        msg     MSG
+endl
+        ; fill the wc structure
         invoke  GetModuleHandle, 0
         mov     [wc.hInstance], eax
         invoke  LoadCursor, 0, IDC_ARROW
         mov     [wc.hCursor], eax
-        invoke  RegisterClass, wc
-        invoke  CreateWindowEx, 0, class_name, window_title, \
+        mov     [wc.style], 0
+        mov     [wc.lpfnWndProc], window_proc
+        mov     [wc.cbClsExtra], 0
+        mov     [wc.cbWndExtra], 0
+        mov     [wc.hIcon], 0
+        mov     [wc.hbrBackground], COLOR_BTNFACE+1
+        mov     [wc.lpszMenuName], 0
+        mov     [wc.lpszClassName], class_name
+        ; register the main window class
+        invoke  RegisterClass, addr wc
+        ; TODO: check error
+        ; reate the main window.
+        invoke  CreateWindowEx, 0, class_name, "kengine", \
                                 WS_VISIBLE+WS_POPUP+WS_SYSMENU, 128, 128,\
                                 [window_width], [window_height], NULL, NULL,\
                                 [wc.hInstance], NULL
+        ; TODO: check error
+        ; main window message loop
         .while
-                invoke  GetMessage, msg, 0, 0, 0
+                invoke  GetMessage, addr msg, 0, 0, 0
                 .if     eax = 0
                         .break
                 .endif
-                invoke  TranslateMessage,msg
-                invoke  DispatchMessage,msg
+                invoke  TranslateMessage, addr msg
+                invoke  DispatchMessage, addr msg
         .endw
-        invoke  ExitProcess,eax
+        ; place the return value to eax
+        mov     eax, [msg.message]
+        ret
+endp
 
 include 'helpers.inc'
 include 'wndproc.inc'
@@ -33,13 +66,12 @@ include 'font.inc'
 .end start
 
 .data
+; process id
 pID               dd          ?
+; process name
 pname             db          'your_process_name.exe',0
 class_name        db          'kengine',0
-window_title      db          'kengine',0
-msg               MSG
-wc                WNDCLASS    0,window_proc,0,0,0,0,0,COLOR_BTNFACE+1,0,\
-                              class_name
+
 ; gui
 canvas            BITMAPINFO
 ps                PAINTSTRUCT
