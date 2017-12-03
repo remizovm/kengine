a6cc23083010ec0be7c25f4bba195cf520e7f018
Mikhail Remizov <00keng@gmail.com>
2017-12-03 18:15:51 +0300

    Game status update timer
    We need a good way to keep the games process id up to date. For example,
    we will not be able to write any memory using invalid process id in case
    if the game was closed. This commit adds a timer and a pid update
    function.
    
    - Add mem_update function which updates the games pid
    - Add on_timer_event function which handles all timer events
    - Add timer IDs for GUI and mem timers
    - Change mem_init so it will just start up the mem_update timer
    - Set GUI timer timeout to 40ms (25fps)
    
    This closes #2.

--- src/kengine.asm
+++ src/kengine.asm
@@ -9,10 +9,10 @@ include 'structs.inc'
 .code
 ; main entry point
 start:
-        invoke  DialogBoxParam, <invoke  GetModuleHandle, 0>, 37, 0,\
-                                dialog_proc, 0
-        ; TODO: check error
-        invoke  ExitProcess,0
+	invoke	DialogBoxParam, <invoke  GetModuleHandle, 0>, 37, 0,\
+				dialog_proc, 0
+	; TODO: check error
+	invoke	ExitProcess,0
 
 include 'helpers.inc'
 include 'dlgproc.inc'
@@ -23,32 +23,32 @@ include 'font.inc'
 
 .data
 ; process id
-pID               dd          ?
+pID		  dd	      ?
 ; process name
-pname             db          'your_process_name.exe',0
-class_name        db          'kengine',0
+pname		  db	      'your_process_name.exe',0
+class_name	  db	      'kengine',0
 
 ; gui
-canvas            BITMAPINFO
-ps                PAINTSTRUCT
-hDC               dd          ?
-canvas_dc         dd          ?
-canvas_bmp        dd          ?
-canvas_buffer     dd          ?
-bpp               dw          32
-window_width      dd          256
-window_height     dd          192
+canvas		  BITMAPINFO
+ps		  PAINTSTRUCT
+hDC		  dd	      ?
+canvas_dc	  dd	      ?
+canvas_bmp	  dd	      ?
+canvas_buffer	  dd	      ?
+bpp		  dw	      32
+window_width	  dd	      256
+window_height	  dd	      192
 
-letters           LETTERS
+letters 	  LETTERS
 
-version           db 'version 1.0.0'
+version 	  db 'version 1.0.0'
 
 section '.rsrc' resource data readable
 
   directory RT_DIALOG,dialogs
 
   resource dialogs,\
-           37,LANG_ENGLISH+SUBLANG_DEFAULT,main
+	   37,LANG_ENGLISH+SUBLANG_DEFAULT,main
 
   dialog main,'kengine',0,0,170,118,WS_POPUP
   enddialog
